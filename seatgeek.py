8c1885c3dd92932558e995c42cdde78d2dde21d1
Madison Leidner <madisonleidner@Madisons-MacBook-Pro-157.local>
2022-12-01 15:39:33 -0500

    city and score info

--- seatgeek.py
+++ seatgeek.py
@@ -1,1 +1,34 @@
+import json
+import os
+import requests
+import sqlite3
+import matplotlib
+
+api_key = "Mjk2NjIyNTN8MTY2OTY3Nzg5Mi40OTk3MTM0"
+
+def get_venue_and_city(first_name, last_name):
+    url = f'https://api.seatgeek.com/2/events?performers.slug={first_name}-{last_name}&client_id={api_key}'
+    try:
+        resp = requests.get(url)
+        data = json.loads(resp.text)
+        #print(data)
+    except:
+        return None
+    tup = []
+    for item in data['events']:
+        city = item['venue']['city']
+        score = item['venue']['score']
+        tup.append((city,score))
+    print(tup)
+    return tup
+
+
+get_venue_and_city("justin", "bieber")
+
+
+"https://api.seatgeek.com/2/events?performers.slug=justin-bieber&client_id=Mjk2NjIyNTN8MTY2OTY3Nzg5Mi40OTk3MTM0"
+
+
+
+
 
