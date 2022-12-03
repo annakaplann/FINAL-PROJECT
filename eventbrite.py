7c1cbf6ba87d3686a0adeddfd6b6d712ac2548ff
Lauren Snerson <laurensnerson@MacBook-Air-210.local>
2022-12-03 11:02:23 -0500

    commit

--- eventbrite.py
+++ eventbrite.py
@@ -6,6 +6,8 @@ import matplotlib
 
 API_KEY = "LZZ5WPD7HVNPP3QQ3KKE"
 
+# https://api.songkick.com/api/3.0/search/venues.json?query={venue_name}&apikey=LZZ5WPD7HVNPP3QQ3KKE
+
 def setUpDatabase(db_name):
     path = os.path.dirname(os.path.abspath(__file__))
     conn = sqlite3.connect(path+'/'+db_name)
@@ -13,34 +15,21 @@ def setUpDatabase(db_name):
     return cur, conn
 
 
-def get_city(event_id):    
-    url = f'https://www.eventbriteapi.com/v3/events/{event_id}/?expand=venue'
+def get_capacity(venue_name):  
+    url = f'https://api.songkick.com/api/3.0/search/venues.json?query={venue_name}&apikey={API_KEY}'
 
-    try: 
-        resp = requests.get(url)
-        data = json.loads(resp.text)
-        return data['city']
+    resp = requests.get(url)
+    data = json.loads(resp.text)
+    print(data)
+    return data['capacity']
 
-    except: 
-        print('Exception')
-        return None
+    # print('Exception')
+    # return None
 
-def get_event(event_id):
-    url = f'https://www.eventbriteapi.com/v3/events/{event_id}/?expand=ticket_classes'
-
-    try: 
-        resp = requests.get(url)
-        data = json.loads(resp.text)
-        return data(['name'],['quantity_sold'])
-
-    except: 
-        print('Exception')
-        return None
-
-def create_city_table(cur,conn,cities):
-    cur.execute("CREATE TABLE IF NOT EXISTS cities_data (id INTEGER PRIMARY KEY, name TEXT)")
+def create_venue_table(cur,conn,cities):
+    cur.execute("CREATE TABLE IF NOT EXISTS venue_data (id INTEGER PRIMARY KEY, name TEXT)")
     dict = []    
-    names = get_city(cities)
+    names = get_capacity(cities)
     try:
         for name in names:
             dict.append(name)
@@ -48,7 +37,7 @@ def create_city_table(cur,conn,cities):
         pass
     try:
         for i in range(1, len(dict)):
-            cur.execute("INSERT OR IGNORE INTO cities_data (id,name) VALUES (?,?)",(i,dict[i-1]))
+            cur.execute("INSERT OR IGNORE INTO venue_data (displayName,capacity) VALUES (?,?)",(i,dict[i-1]))
             conn.commit()
     except:
         pass
@@ -56,8 +45,8 @@ def create_city_table(cur,conn,cities):
 def main():
     cur, conn = setUpDatabase('database.db')
     event_id = 3003
-    cities = get_city(event_id)
-    create_city_table(cur,conn,cities)
+    cities = get_capacity(event_id)
+    create_venue_table(cur,conn,cities)
     print('Done')
 
     
