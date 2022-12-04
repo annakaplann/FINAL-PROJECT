ed7ff61adac9ae693076e4e159d59204d2dcfde4
Anna Kaplan <annakap@umich.edu>
2022-12-04 18:35:05 -0500

    Finished creating database, table, and limiting data

--- wiki.py
+++ wiki.py
@@ -10,32 +10,17 @@ def concert_data():
     resp = requests.get('https://en.wikipedia.org/wiki/Love_On_Tour')
     soup = BeautifulSoup(resp.text, 'html.parser')
     table_tags = soup.find_all('table', class_ = 'wikitable plainrowheaders')
-
-    #HARRY STYLES LOVE ON TOUR 2021 & 2022
-    for table in table_tags:
-        for concert in table.find_all('tr'):
-            concert_info = concert.find_all('td')
-            if concert_info != []:
-                city = concert_info[0].text.strip()
-                pattern = "(\d*,\d*) / .*"
-                tickets = concert_info[-2].text.strip()
-                tickets_sold = re.findall(pattern, tickets)
-                pattern2 = ".* / (\d*,\d*)"
-                capacity = re.findall(pattern2, tickets)
-                if tickets_sold != []:
-                    number = re.sub(",", "", tickets_sold[0])
-                    number2 = re.sub(",", "", capacity[0])
-                    print([city, int(number), int(number2)])
-
     resp2 = requests.get('https://en.wikipedia.org/wiki/Harry_Styles:_Live_on_Tour')
     soup = BeautifulSoup(resp2.text, 'html.parser')
     table_tags2 = soup.find_all('table', class_ = 'wikitable')
+    data = []
 
     #HARRY STYLES LIVE ON TOUR 2017 & 2018
     for table in table_tags2:
         for concert in table.find_all('tr'):
             concert_info = concert.find_all('td')
             if len(concert_info) >= 5 and len(concert_info) <= 7:
+                date = concert_info[0].text.strip()
                 city = concert_info[1].text.strip()
                 pattern = "(\d*,\d*) / .*"
                 tickets = concert_info[-2].text.strip()
@@ -45,6 +30,55 @@ def concert_data():
                 if tickets_sold != []:
                     number = re.sub(",", "", tickets_sold[0])
                     number2 = re.sub(",", "", capacity[0])
-                    print([city, int(number), int(number2)])
-    
-concert_data()
+                    data.append([date, city, int(number), int(number2)])
+
+    #HARRY STYLES LOVE ON TOUR 2021 & 2022
+    for table in table_tags:
+        for concert in table.find_all('tr'):
+            concert_date = concert.find_all('th')
+            concert_info = concert.find_all('td')
+            if concert_info != []:
+                date = concert_date[0].text.strip()
+                city = concert_info[0].text.strip()
+                pattern = "(\d*,\d*) / .*"
+                tickets = concert_info[-2].text.strip()
+                tickets_sold = re.findall(pattern, tickets)
+                pattern2 = ".* / (\d*,\d*)"
+                capacity = re.findall(pattern2, tickets)
+                if tickets_sold != []:
+                    number = re.sub(",", "", tickets_sold[0])
+                    number2 = re.sub(",", "", capacity[0])
+                    data.append([date, city, int(number), int(number2)])
+    return data
+
+def make_database(database_name):
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path+'/'+database_name)
+    cur = conn.cursor()
+    return cur, conn
+
+def make_table(data, cur, conn):
+    cur.execute("CREATE TABLE IF NOT EXISTS Harry_Styles (date TEXT PRIMARY KEY, city TEXT, attendance INTEGER, capacity INTEGER)")
+    limit = 0
+    for concert in data:
+        if limit < 25:
+            date = concert[0]
+            city = concert[1]
+            attendance = concert[2]
+            capacity = concert[3]
+            rows = cur.execute("SELECT COUNT(*) FROM Harry_Styles")
+            num = rows.fetchone()[0]
+            cur.execute("INSERT OR IGNORE INTO Harry_Styles (date, city, attendance, capacity) VALUES (?, ?, ?, ?)", (date, city, attendance, capacity))
+            rows2 = cur.execute("SELECT COUNT(*) FROM Harry_Styles")
+            num2 = rows2.fetchone()[0]
+            if num2 > num:
+                limit += 1
+            conn.commit()
+
+
+def main():
+    harry_data = concert_data()
+    cur, conn = make_database('concerts.db')
+    make_table(harry_data, cur, conn)
+
+main()
